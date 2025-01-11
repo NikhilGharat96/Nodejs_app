2c9c1be7521485374228a6010e274f000f690dc0
NikhilGharat96 <nikhilgharat1718@gmail.com>
2025-01-12 00:16:11 +0530

    newww commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
  
 var app = express();//Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('nahi disla n bhai');
+ res.send('disla n bhai');
 });//listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
  
